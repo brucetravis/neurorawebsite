c32c69e11921060b1bc6228701cb4ae3294ad7aa
Bruce Ambundo <bruceambundo@gmail.com>
2026-04-04 12:42:36 +0300

    AI Platform page complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Landing from './pages/landing/Landing';
 import SoftwarePage from './pages/software/SoftwarePage';
+import AIPlatform from './pages/platform/AIPlatform';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Landing />} />
         <Route path='/software' element={<SoftwarePage />} />
+        <Route path='/platform' element={<AIPlatform />} />
       </Routes>
     </div>
   );
